6cb8c9e37b43a4b44632ab8def8986543d062d9e
mambitoano2000 <joaocostagaspar@gmail.com>
2021-05-13 10:51:32 +0200

    working in html product page

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function buildColsForProductType(value, productType, row){
     let col = `<div class="col">
     <a href="/product.html?type=${productType}&id=${value[i]._id}"><div class="card">
                          <div class="card-body">
-                         <img src="${value[i].imageUrl}">  
+                         <img src="${value[i].imageUrl}" class="img-fluid">  
                         <h5 class="card-title" >${value[i].name}</h5>
                        </div>
                        </div></a>
@@ -38,8 +38,8 @@ function fetchProducts(url, productType, row) {
 // Calling fetch function to catch data and create the 3 html rows of products
 
 fetchProducts('http://localhost:3000/api/furniture/', 'furniture', 'meublesRow');
-fetchProducts('http://localhost:3000/api/teddies/', 'teddy', 'teddiesRow' );
-fetchProducts('http://localhost:3000/api/cameras', 'camera', 'camerasRow' );
+fetchProducts('http://localhost:3000/api/teddies/', 'teddies', 'teddiesRow' );
+fetchProducts('http://localhost:3000/api/cameras', 'cameras', 'camerasRow' );
 
 
 // product get query string
@@ -62,18 +62,24 @@ console.log(id);
 
 
 
-// product.html
-fetch(`http://localhost:3000/api/${type}/${id}`)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-    buildColsForProductType(value, productType, row);
-  
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
+// product.html fetch
+//fetch(`http://localhost:3000/api/${type}/${id}`)
+//  .then(function(res) {
+//    if (res.ok) {
+//      return res.json();
+//    }
+//  })
+//  .then(function(value) {
+//    console.log(value);
+//    document
+//        .getElementById(productCard)
+//         productCard.innerHTML = `<ul><li>${value.name}</li><li>Elément 2</li></ul>`
+//  
+//  
+//  })
+//  .catch(function(err) {
+//    // Une erreur est survenue
+//  });
+
+
+
