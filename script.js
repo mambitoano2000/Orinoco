fc40435ef5e5a1d83ec174b0be4ce211ad553888
mambitoano2000 <joaocostagaspar@gmail.com>
2021-05-27 09:43:16 +0200

    localstorage with qnty and option

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Build  columns function
 
-function buildColsForProductType(value, optionType, productType, row){
+function buildColsForProductType(value, optionType, productType, row) {
   let cols = document.getElementById(row)
 
-  for (let i = 0; i < value.length; i++){
+  for (let i = 0; i < value.length; i++) {
     let col = `<div class="col">
     <a href="/product.html?option=${optionType}&type=${productType}&id=${value[i]._id}"><div class="card">
                          <div class="card-body">
@@ -12,7 +12,7 @@ function buildColsForProductType(value, optionType, productType, row){
                        </div>
                        </div></a>
                        </div>`
-    cols.innerHTML += col                
+    cols.innerHTML += col
 
   }
 }
@@ -20,26 +20,26 @@ function buildColsForProductType(value, optionType, productType, row){
 // Get API products
 function fetchProducts(url, optionType, productType, row) {
   fetch(url)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-    buildColsForProductType(value, optionType, productType, row);
-  
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      console.log(value);
+      buildColsForProductType(value, optionType, productType, row);
+
+    })
+    .catch(function (err) {
+      // Une erreur est survenue
+    });
 }
 
 // Calling fetch function to catch data and create the 3 html rows of products
 
 fetchProducts('http://localhost:3000/api/furniture/', 'varnish', 'furniture', 'meublesRow');
-fetchProducts('http://localhost:3000/api/teddies/', 'colors', 'teddies', 'teddiesRow' );
-fetchProducts('http://localhost:3000/api/cameras', 'lenses', 'cameras', 'camerasRow' );
+fetchProducts('http://localhost:3000/api/teddies/', 'colors', 'teddies', 'teddiesRow');
+fetchProducts('http://localhost:3000/api/cameras', 'lenses', 'cameras', 'camerasRow');
 
 
 // product get query string
@@ -68,116 +68,120 @@ console.log(id);
 //product.html fetch
 
 fetch(`http://localhost:3000/api/${type}/${id}`)
- .then(function(res) {
-   if (res.ok) {
-     return res.json();
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
     }
   })
-  .then(function(productValue) {
+  .then(function (productValue) {
     console.log(productValue);
 
     document
-          .getElementById(productName)
-          productName.textContent = `${productValue.name}`;
-   document
-          .getElementById(productPrice)
-          productPrice.textContent = `Prix: ${productValue.price}$`;
-   document       
-          .getElementById(productDescription)
-          productDescription.textContent = `${productValue.description}`;
-
-  document 
-          .getElementById(productImage)
-           productImage.src = productValue.imageUrl;   
-           
-document
-          .getElementById(productQuantityDiv);  
-           productQuantityDiv.innerHTML = `<label for="quantity">Quantité:</label>
-           <input type="number" id="productQuantity" name="quantity"  value="1" min="1"  placeholder="1">`; 
-
-// document
-//          .getElementById(addToCartBtn);
-//          addToCartBtn.innerHTML = `<button type="button" id="addToCart" class="btn btn-primary mt-3">Ajouter au panier</button>`;            
-
- //let productQuantity = document.getElementById('productQuantity').value;
-//console.log("product quantity: ", productQuantity); 
-
-// Get Add to Cart button and attach click event
-let btnSubmit = document.getElementById('addToCart');
-btnSubmit.addEventListener('click', function() {
-  // Get product data
-  let productQuantity = document.getElementById('productQuantity').value;
-  let selectedProperties = {
-    quantity: productQuantity,
-  }
-  sendProductToLocalStorage(productValue, selectedProperties);
-});
-
-
- if (type === 'teddies') {
+      .getElementById(productName)
+    productName.textContent = `${productValue.name}`;
     document
-        .getElementById(optionType)
-         optionType.textContent = "Couleurs:";
-         buildInputForOptionsInput(productValue, 'colors');
- 
- }
-
-else if (type === 'cameras') {
+      .getElementById(productPrice)
+    productPrice.textContent = `Prix: ${productValue.price}$`;
     document
+      .getElementById(productDescription)
+    productDescription.textContent = `${productValue.description}`;
+
+    document
+      .getElementById(productImage)
+    productImage.src = productValue.imageUrl;
+
+   /* document
+      .getElementById(productQuantityDiv);
+    productQuantityDiv.innerHTML = `<label for="quantity">Quantité:</label>
+           <input type="number" id="productQuantity" name="quantity"  value="1" min="1"  placeholder="1">`;*/
+
+
+    if (type === 'teddies') {
+      document
         .getElementById(optionType)
-         optionType.textContent = "Lentilles:";
-         buildInputForOptionsInput(productValue, 'lenses');
+      optionType.textContent = "Couleurs:";
+      buildInputForOptionsInput(productValue, 'colors');
 
-}  
+    }
 
-
-else if (type === 'furniture') {
-     document
+    else if (type === 'cameras') {
+      document
         .getElementById(optionType)
-         optionType.textContent = "Vernis:";
-         buildInputForOptionsInput(productValue, 'varnish');
-  
-}})
+      optionType.textContent = "Lentilles:";
+      buildInputForOptionsInput(productValue, 'lenses');
 
-  .catch(function(err) {
+    }
+
+
+    else if (type === 'furniture') {
+      document
+        .getElementById(optionType)
+      optionType.textContent = "Vernis:";
+      buildInputForOptionsInput(productValue, 'varnish');
+
+    }
+      // Create quantity and submit btn
+    document
+      .getElementById(optionForm);
+    let quantityAndSubmitBtns = `<label for="quantity">Quantité:</label>
+           <input type="number" id="productQuantity" name="quantity"  value="1" min="1"  placeholder="1"><br><button type="submit" id="addToCart" class="btn btn-primary mt-3">Ajouter au panier</button>`;
+    optionForm.innerHTML += quantityAndSubmitBtns;
+
+    let onSubmit = function () {
+      // Get product data
+      let productQuantity = document.getElementById('productQuantity').value;
+
+      let productColor = document.querySelector('input[name="productoptions"]:checked').value;
+      console.log('productColor: ', productColor);
+
+      let selectedProperties = {
+        quantity: productQuantity,
+      }
+      sendProductToLocalStorage(productValue, selectedProperties);
+    }
+
+    // Get Add to Cart button and attach click event
+    // let btnSubmit = document.getElementById('addToCart');
+    // btnSubmit.addEventListener('click', onSubmit);
+
+    function logSubmit(event) {
+      event.preventDefault();
+      console.log('SUBMIT!!!')
+      console.log(event.target.elements);
+      console.log(event.target.elements.productoptions.value);
+      console.log(event.target.elements.quantity.value);
+    }
+    
+    const form = document.getElementById('optionForm');
+    form.addEventListener('submit', logSubmit);
+  })
+
+  .catch(function (err) {
     // Une erreur est survenue
   })
 
 // Function to create options input
 
-function buildInputForOptionsInput(value, type){
+function buildInputForOptionsInput(value, type) {
   let optionForm = document.getElementById('optionForm')
   console.log("type: ", type);
   let options = value[type]
-  for (let i = 0; i < options.length; i++){
+  for (let i = 0; i < options.length; i++) {
     console.log("log options: ", value);
     let inputAndLabel = `<input type="radio" name="productoptions" value="${options[i]}" ${i === 0 ? 'checked' : ''}>
     <label for="${options[i]}">${options[i]}</label><br>`
-    optionForm.innerHTML += inputAndLabel                
+    optionForm.innerHTML += inputAndLabel
 
   }
 }
 
-// product click event
-
-  // let btnClickEvent = document.getElementById('addToCart');
-  // btnClickEvent.addEventListener('click', function() {   
-    
-  //    productQuantity = document.getElementById('productQuantity').value;
-  //   console.log("product quantity: ", productQuantity);
-  // });
-
-
-
-
-  
 
 // send product and quantity to localStorage
 
 function sendProductToLocalStorage(product, selectedProperties) {
   console.log('product: ', product);
   console.log('selectedProperties: ', selectedProperties);
-  let itemToSave = { 
+  let itemToSave = {
     ...product,
     ...selectedProperties,
   }
